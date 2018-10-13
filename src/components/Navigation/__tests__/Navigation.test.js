15bbd0e707ccee97509a7d434ae569fababf271b
Filip Messa <filip.messa@kiwi.com>
2018-10-13 12:07:54 +0200

    General: update jest-dom

--- src/components/Navigation/__tests__/Navigation.test.js
+++ src/components/Navigation/__tests__/Navigation.test.js
@@ -7,33 +7,59 @@ describe('Navigation', () => {
   test('render', () => {
     const { baseElement } = render(<Navigation />)
     expect(baseElement).toMatchInlineSnapshot(`
+.c0 {
+  padding: 2px 0;
+}
+
+.c0 a {
+  color: #030303;
+  -webkit-text-decoration: none;
+  text-decoration: none;
+}
+
+.c0 a:hover,
+.c0 a:focus,
+.c0 a:active {
+  -webkit-text-decoration: none;
+  text-decoration: none;
+  color: #B5B5B5;
+}
+
 <body>
   <div>
-    <ul
-      class=""
-    >
-      <li>
-        <a
-          href="#"
-        >
-          Page 1
-        </a>
-      </li>
-      <li>
-        <a
-          href="#"
-        >
-          Page 2
-        </a>
-      </li>
-      <li>
-        <a
-          href="#"
-        >
-          Page 3
-        </a>
-      </li>
-    </ul>
+    <nav>
+      <ul
+        class=""
+      >
+        <li>
+          <li
+            class="c0"
+          >
+            <link>
+              Page 1
+            </link>
+          </li>
+        </li>
+        <li>
+          <li
+            class="c0"
+          >
+            <link>
+              Page 2
+            </link>
+          </li>
+        </li>
+        <li>
+          <li
+            class="c0"
+          >
+            <link>
+              Page 3
+            </link>
+          </li>
+        </li>
+      </ul>
+    </nav>
   </div>
 </body>
 `)
