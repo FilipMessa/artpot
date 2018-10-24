414db521e343f43071d21d375fc10313a61f9da8
Filip Messa <filip.messa@kiwi.com>
2018-10-24 16:22:05 +0200

    General: add gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,15 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
     {
       resolve: 'gatsby-plugin-eslint',
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,54 @@
 
 import * as React from 'react'
 import styled from 'styled-components'
+import Img from 'gatsby-image'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 
-const Image = styled.img`
-  max-width: 100%;
+type Fluid = {
+  base64: string,
+  aspectRatio: number,
+  src: string,
+  srcSet: string,
+  sizes: string,
+}
+
+type ChildImageSharp = {
+  fluid: Fluid,
+}
+
+type IntroImage = {
+  childImageSharp: ChildImageSharp,
+}
+
+type Data = {
+  introImage: IntroImage,
+}
+
+const ImageWrapper = styled.div`
+  max-width: 600px;
   margin: 3em 0;
 `
 
-const IndexPage = () => (
-  <Layout>
-    <Image src="http://petravojtekova.com/assets/Uploads/_resampled/SetWidth800-the-Mound.jpg" />
-  </Layout>
-)
+const IndexPage = ({ data }: { data: Data }) => (
+    <Layout>
+      <ImageWrapper>
+        <Img fluid={data.introImage.childImageSharp.fluid} />
+      </ImageWrapper>
+    </Layout>
+  )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    introImage: file(relativePath: { eq: "intro.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
