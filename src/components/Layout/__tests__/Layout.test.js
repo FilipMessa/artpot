9c6b0b3f830702511bf3c5a6e991a5f4c54cc285
Filip Messa <filip.messa@kiwi.com>
2018-11-21 21:10:44 +0100

    General: update packages

--- src/components/Layout/__tests__/Layout.test.js
+++ src/components/Layout/__tests__/Layout.test.js
@@ -1,10 +1,11 @@
 // @flow
+/* eslint-disable no-console */
 import React from 'react'
 import { render } from 'react-testing-library'
 
 import { PureLayout as Layout } from '../index'
 
-// eslint-disable-next-line no-console
+// $FlowTest
 console.error = jest.fn()
 
 describe('Layout', () => {
