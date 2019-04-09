7ce82e1165b99d691b0181edc64a2073eb2f3c96
Filip Messa <messa.filip@gmail.com>
2019-04-09 17:46:49 +0200

    add new pages

---
+++ src/pages/works.js
@@ -0,0 +1,41 @@
+// @flow
+
+import * as React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+import Poster from '../components/Poster'
+
+type Fluid = {
+  base64: string,
+  aspectRatio: number,
+  src: string,
+  srcSet: string,
+  sizes: string,
+}
+
+type ChildImageSharp = {
+  fluid: Fluid,
+}
+
+type Image = {
+  childImageSharp: ChildImageSharp,
+}
+
+type Data = {
+  poster: Image,
+}
+
+const IndexPage = ({ data }: { data: Data }) => (
+  <Layout>
+    <h1>Works Page</h1>
+  </Layout>
+)
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    ...SiteInformation
+  }
+`
