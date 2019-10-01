01a7779bb57a256e1731bc870e224825ed58f69c
Filip Messa <messa.filip@gmail.com>
2019-10-01 19:40:34 +0200

    update dependencies (#11)

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,7 +2,3 @@
 
 export { default as ModalRoot } from './ModalRoot'
 export { default as Modal } from './Modal'
-
-//
-// 1. Prepare document about differencies between web and RNW.
-// 2Podporujue className ale su oznacene v zdorjaku ako depricated
