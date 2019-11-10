3fdf7b32db1328ca1403104c2b7f280833663438
Filip Messa <messa.filip@gmail.com>
2019-11-10 11:11:20 +0100

    ...progress on lightbox

--- src/components/Lightbox/Lightbox.js
+++ src/components/Lightbox/Lightbox.js
@@ -17,23 +17,21 @@ const Content = styled.div`
 `
 
 export function Lightbox({ children }) {
+  const handleLeftClick = e => {
+    e.stopPropagation()
+    console.log('LEFT')
+  }
+
+  const handleRightClick = e => {
+    e.stopPropagation()
+    console.log('RIGHT')
+  }
+
   return (
     <Container>
-      <NextItem
-        type={DIRECTION_TYPES.LEFT}
-        onClick={e => {
-          e.stopPropagation()
-          console.log('LEFT')
-        }}
-      />
+      <NextItem type={DIRECTION_TYPES.LEFT} onClick={handleLeftClick} />
       <Content>{children}</Content>
-      <NextItem
-        type={DIRECTION_TYPES.RIGHT}
-        onClick={e => {
-          e.stopPropagation()
-          console.log('RIGHT')
-        }}
-      />
+      <NextItem type={DIRECTION_TYPES.RIGHT} onClick={handleRightClick} />
     </Container>
   )
 }
