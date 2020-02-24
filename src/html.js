983ae3f7251584d13bba838c5bd2e7ee6ee333bd
Filip Messa <messa.filip@gmail.com>
2020-02-24 09:37:03 +0100

    add rel="preconnect" for fonts

--- src/html.js
+++ src/html.js
@@ -13,6 +13,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
