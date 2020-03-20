c0182d39e6683ebe74b90bb30426ebf28f654b82
Filip Messa <messa.filip@gmail.com>
2020-03-20 17:00:58 +0100

    add description for gallery pages

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Truncate from 'react-truncate'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { media } from '@theme'
 import Text from '../Text'
 import Button from './components/Button'
+
+const Paragraph = styled.p`
+  margin: 2.1em 0;
+  ${media.greaterThan('medium')`
+    margin-left: 1.3em;
+    margin-right: 1.3em;
+  `}
+`
 
 const ReadMore = ({ text, lines = 3 }) => {
   const [isExpanded, setExpanded] = React.useState(false)
   const toggleLines = () => setExpanded(!isExpanded)
 
   return (
-    <>
+    <Paragraph>
       <Truncate
         lines={!isExpanded && lines}
         ellipsis={
@@ -23,7 +33,7 @@ const ReadMore = ({ text, lines = 3 }) => {
       </Truncate>
       {` `}
       {isExpanded && <Button text="(read less)" onClick={toggleLines} />}
-    </>
+    </Paragraph>
   )
 }
 
